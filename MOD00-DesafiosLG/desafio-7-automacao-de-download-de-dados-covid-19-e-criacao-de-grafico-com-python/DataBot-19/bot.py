ba13664c6acbb45fd2fa9b298c524f264c649eb0
Jade Santos <santmjad@gmail.com>
2024-10-31 20:11:39 -0400

    [FEAT] gerando gráfico com os dados extraídos da COVID-19

--- MOD00-DesafiosLG/desafio-7-automacao-de-download-de-dados-covid-19-e-criacao-de-grafico-com-python/DataBot-19/bot.py
+++ MOD00-DesafiosLG/desafio-7-automacao-de-download-de-dados-covid-19-e-criacao-de-grafico-com-python/DataBot-19/bot.py
@@ -4,10 +4,11 @@ from botcity.plugins.googlesheets import BotGoogleSheetsPlugin
 from webdriver_manager.chrome import ChromeDriverManager
 from owid import catalog
 from datetime import datetime
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
 import pandas as pd
 
 BotMaestroSDK.RAISE_NOT_CONNECTED = False
-
 
 class bot(WebBot):
 
@@ -24,7 +25,7 @@ class bot(WebBot):
         print(f"Task Parameters are: {execution.parameters}")
 
         try:
-            # Carrega o catálogo de dados
+            # Carrega o catálogo de dados OWI
             rc = catalog.RemoteCatalog()
             uri = 'garden/covid/latest/cases_deaths/cases_deaths'
             df = rc[uri]
@@ -42,7 +43,6 @@ class bot(WebBot):
                 "days_since_100_total_cases_and_5m_pop"
             ]
             df_filtered = df[selected_columns].copy()
-            print('teste')
 
             # Filtra para o mês atual e para o Brasil
             current_month = datetime.now().month
@@ -54,11 +54,8 @@ class bot(WebBot):
                 (df_filtered['country'] == 'Brazil')
             ]
 
-            print(df_filtered)
-
-            # Inicializa o plugin do Google Sheets
+            # Adicionar dados filtrados no google sheets
             bot_planilha = BotGoogleSheetsPlugin('credential.json', '1Co2Yuuj23NDZ78GClNFlMebSm5Mcn7x4V5zDBWsbIeM')
-
             for _, row in df_filtered.iterrows():
                 bot_planilha.add_rows([[
                     row['country'],
@@ -69,7 +66,37 @@ class bot(WebBot):
                     row['days_since_0_1_total_deaths_per_million'],
                     row['days_since_100_total_cases_and_5m_pop']
                 ]])
-        
+
+            #Gerar grafico
+            columns = [
+                ('days_since_100_total_cases', "Days since 100 total cases"),
+                ('days_since_5_total_deaths', "Days since 5 total deaths"),
+                ('days_since_1_total_cases_per_million', "Days since 1 case per million"),
+                ('days_since_0_1_total_deaths_per_million', "Days since 0.1 deaths per million"),
+                ('days_since_100_total_cases_and_5m_pop', "Days since 100 total cases and 5M pop")
+            ]
+
+            # Loop para gerar um gráfico para cada coluna
+            for col, title in columns:
+                plt.figure(figsize=(10, 6))
+                plt.plot(df_filtered['date'], df_filtered[col], label=title, color='b')
+                
+                # Personalizando o gráfico
+                plt.xlabel("Date")
+                plt.ylabel("Days Since Milestone")
+                plt.title(f"Daily Evolution of COVID-19: {title}")
+                plt.legend()
+                
+                # Formatação das datas no eixo X
+                plt.gca().xaxis.set_major_locator(mdates.DayLocator(interval=1))
+                plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
+                plt.xticks(rotation=45)
+                plt.tight_layout()
+
+                # Salva cada gráfico como uma imagem
+                plt.savefig(f'{col}_grafico_covid_brasil.png')
+                plt.close()
+                    
         except Exception as erro:
             print('Erro: ', erro)
             self.save_screenshot('erro.png')
@@ -87,7 +114,6 @@ class bot(WebBot):
 
     def not_found(self, label):
         print(f"Element not found: {label}")
-
 
 if __name__ == '__main__':
     bot.main()
